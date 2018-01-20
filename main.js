23c1e7e5ba7a72500b5fae9b15ae47c45cd297f6
Barylo Aleks <baryloaleks@gmail.com>
2018-01-20 21:06:28 +0200

    Almost ready. Needto fix symols select and audio sounds

---
+++ main.js
@@ -0,0 +1,42 @@
+const playSound = note => {
+    const audio = document.querySelector(`audio[data-note=${note}]`);
+    audio.currentTime = 0;
+    audio.play();
+};
+
+const buttons = Array.from(document.querySelectorAll("button"));
+const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./".split("");
+const SPACE = document.querySelector('.keyboard__btn--space');
+
+const buttonPress = (event) => {
+    if (event.code == "Space") {
+        flashing(SPACE);
+    } 
+    
+    else {
+        let key = event.code.slice(-1).toString().toLowerCase();
+        console.log(key);
+        console.log(event);
+        buttons.forEach(function (item, i, arr) {
+            if (item.innerHTML == key) {
+                flashing(item);
+                playSound(sound(item));
+            }
+        });
+    } 
+    
+}
+
+window.addEventListener('keydown', buttonPress);
+
+
+let flashing = function (element) {
+    element.classList.add('keyboard__btn--active');
+    setTimeout(() => { element.classList.remove('keyboard__btn--active') }, 500);
+}
+
+let sound = (button) => {
+    note = button.getAttribute('data-note')
+    console.log(note);
+    return note;
+}
